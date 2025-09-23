c103ebc022b3b7b511ff74804fd5a6207226fdf1
Natalie Danner <danner15@usf.edu>
2025-09-23 13:31:27 -0400

    Added functions and started on payroll

--- cc_5.js
+++ cc_5.js
@@ -19,3 +19,31 @@ let employees = [
     },
 ];
 
+// Function for base pay
+
+function calculateBasePay(rate, hours){
+    return rate * 40
+}
+
+// Function for overtime pay
+
+function calculateOvertimePay(rate, hours){
+    if (hours > 40) return (1.5 * rate) * (hours - 40)
+        else return 0
+}
+
+// Function deducting taxes
+
+let grossPay = calculateBasePay() + calculateOvertimePay()
+function calculateTaxes(grossPay){
+    return grossPay * 0.85
+}
+
+// Function for processing payroll
+
+function processPayroll(employee){
+    return console.log(`Name: ${employee.name}, Base Pay: $${calculateBasePay(employee.hourlyRate, employee.hoursWorked)}, 
+    Overtime Pay: $${calculateOvertimePay(employee.hourlyRate, employee.hoursWorked)}, Gross Pay: $${calculateBasePay(employee.hourlyRate, employee.hoursWorked) + calculateOvertimePay(employee.hourlyRate, employee.hoursWorked)}`)
+}
+
+processPayroll(employees[0]);
