c3388834d95d311f98b1794b523307aaeceadfff
Natalie Danner <danner15@usf.edu>
2025-09-23 12:51:44 -0400

    first commit

---
+++ cc_5.js
@@ -0,0 +1,21 @@
+// Coding Challenge 5
+
+// Create array of employee objects
+let employees = [
+    {
+        name: "Olivia Johnson",
+        hourlyRate: 30,
+        hoursWorked: 40,
+    },
+     {
+        name: "Rachel James",
+        hourlyRate: 40,
+        hoursWorked: 45,
+    },
+     {
+        name: "Richard Smith",
+        hourlyRate: 35,
+        hoursWorked: 42,
+    },
+];
+
